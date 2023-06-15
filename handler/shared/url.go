4b2af7ce9a0acff37e6bc3f12ab100f3ff6d8d54
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-06-15 21:36:36 +0900

    画像を追加

--- handler/shared/url.go
+++ handler/shared/url.go
@@ -12,18 +12,26 @@ var ImageURLs = []string{
 	"https://cdn.discordapp.com/attachments/984614055681613867/1081373428780642324/dm-off-en.png",
 	// IZUMOのAilisのGIF
 	"https://cdn-longterm.mee6.xyz/plugins/embeds/images/972816072313012297/edea5a9d544353eefa807712622483a28a2aeb47aed387084feadbf60110e0ce.gif",
-	// IZUMOのAilisの名前の正解時
-	"https://cdn.discordapp.com/attachments/1103240223376293938/1106943368749121636/Yay.png",
 	// IZUMOのOnbordingの最後
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1106945011205029968/welcome3.gif",
 	// THE ANARCHYのロゴ
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1115225765542363166/anarchy.jpg",
-	// IZUMOのconnectコマンド
-	"https://cdn.discordapp.com/attachments/1103240223376293938/1115276344708825108/2023-06-05_22.49.53.png",
-	// IZUMO QuestのSTEP1の画像（Ailisおじぎ）
+	// IZUMO（Ailis Yay!!）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1106943368749121636/Yay.png",
+	// IZUMO（Ailisおじぎ）
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1115549494520578098/09_512px.png",
-	// IZUMO QuestのSTEP3の画像（Ailisおじぎ）
+	// IZUMO（Ailisハテナ）
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1115550098647171072/12_512px_1.png",
+	// IZUMO（Ailis Hi）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1118880369383588010/Hi.png",
+	// IZUMO（Ailis にやにや）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1118880685965459487/Smirk.png",
+	// IZUMO（Ailis 音符）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1118881108487049226/Hehe__background.png",
+	// IZUMO（Ailis Congrats!!）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1118881327614271569/Congrats.png",
+	// IZUMO（Ailis Thank you）
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1118881570254753882/Thankyou.png",
 	// アナーキー: ガチャ画像
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1116312631721066517/title_3.png",
 	// アナーキー: カプセルオープン
