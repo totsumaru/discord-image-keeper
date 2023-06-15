38f467044c276f3d8826b9adef83ba295ee6d94b
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-06-16 07:56:23 +0900

    画像を追加

--- handler/shared/url.go
+++ handler/shared/url.go
@@ -46,6 +46,8 @@ var ImageURLs = []string{
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1116312806598389771/hazure.png",
 	// アナーキー: はずれ2
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1118010136762519642/hazure_02.png",
+	// アナーキー: はずれ2
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1119037463650914344/hazure_03.png",
 	// アナーキー: 当たり
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1116312750277263390/atari.png",
 }
