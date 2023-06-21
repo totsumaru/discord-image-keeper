9cadb98a60a7eb5bdfa9e7d0cf6c164731f366ba
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-06-21 15:59:22 +0900

    はずれ画像を追加

--- handler/shared/url.go
+++ handler/shared/url.go
@@ -46,8 +46,10 @@ var ImageURLs = []string{
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1116312806598389771/hazure.png",
 	// アナーキー: はずれ2
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1118010136762519642/hazure_02.png",
-	// アナーキー: はずれ2
+	// アナーキー: はずれ3
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1119037463650914344/hazure_03.png",
+	// アナーキー: はずれ4
+	"https://cdn.discordapp.com/attachments/1103240223376293938/1120971084737560627/hazure_04.png",
 	// アナーキー: 当たり
 	"https://cdn.discordapp.com/attachments/1103240223376293938/1116312750277263390/atari.png",
 }
